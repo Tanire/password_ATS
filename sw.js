bff87ef00afc99b844fda6836ed81778d9ae56ca
Tanire <Ojazos1986@gmail.com>
2026-06-05 12:10:19 +0200

    Bump cache version to v1.05.1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Service Worker for Offline Cache
  */
 
-const CACHE_NAME = "ats-tec-cache-v1.05";
+const CACHE_NAME = "ats-tec-cache-v1.05.1";
 const ASSETS = [
     "index.html",
     "style.css",
